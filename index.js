8d25b222aacd0e3773693bb6489b86520d47c2e4
Nathan Huse <nathan.huse@yahoo.com>
2021-05-26 16:46:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ function updateObjectWithKeyAndValue(object, key, value) {
   newObj[key] = value;
   return newObj;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  const newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
